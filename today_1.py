989c5b73a0eecdbfec41edd8305ead370839b83f
chetankolhe39 <chetankolhe39@gmail.com>
2018-03-28 11:52:49 +0530

    added today_1.py

---
+++ today_1.py
@@ -0,0 +1,53 @@
+import pytest
+import copy
+import math
+
+dlist = []
+d = {}
+d1 = {}
+
+@pytest.fixture()
+def insert_record(x,y,z):
+    d1['name'] = y
+    d1['address'] = z
+    d[x] = copy.deepcopy(d1)
+    dlist.append(copy.deepcopy(d))
+    for i in dlist:
+        if x in i.keys():
+           return "True"
+
+@pytest.mark.parametrize("x,y,z,expected",[
+   (1,'Chetan','Pune',"True"),
+   (2,'Yash','Bsl',"True"),
+   (3,'Ujwal','Akurdi',"True"),
+])
+def test_ins(x,y,z,expected,insert_record):
+    assert insert_record == expected
+
+
+
+@pytest.fixture()
+def search_record(x):
+    for i in dlist:
+        if x in i.keys():
+            return i
+
+@pytest.mark.parametrize("x,expected",[
+   (1,{1:{'name':'Chetan','address':'Pune'}}),
+])
+def test_op(x,expected,search_record):
+    assert search_record == expected
+
+
+@pytest.fixture()
+def delete_record(x):
+    for i in dlist:
+        if x in i.keys():
+            dlist.remove(i)
+            return "True"
+
+@pytest.mark.parametrize("x,expected",[
+   (1,"True"),
+])
+def test_del(x,expected,delete_record):
+    assert delete_record == expected
